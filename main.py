b5f9f97d0b7ae92f37bf4e7d4ccc936e33262ca5
MichaelShults <mishults@gmail.com>
2022-09-08 14:32:17 +0300

    Redefining classes for better encapsulation, code still doesn't run!

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-import sys, pygame
-# returns touple
-def add_pairs(p1, p2):
-    return p1[0] + p2[0], p1[1] + p2[1]
-def scale_pair(p, factor1, factor2):
-    return factor1 * p[0], factor2 * p[1]
-class Snake:
-    def __init__(self):
-        self.part_list = [(0, 0), (0, 10), (0, 20)]
-        self.dir = (0, 10)
+import sys
+import pygame
+BLOCK_SIZE = 10
+#in place
+class NumPair:
+    def __init__(self, c1, c2):
+        self.c1 = c1
+        self.c2 = c2
 
-    def move(self):
-        if len(self.part_list) == 1:
-            self.part_list.append(add_pairs(self.part_list[0], dir))
-        else:
-            last = self.part_list[-1]
-            pre_last = self.part_list[-2]
-            self.dir = add_pairs(last, scale_pair(pre_last, -1, -1))
-            self.part_list.append(add_pairs(last, self.dir))
-        self.part_list.pop(0)
+    def add(self, other):
+        self.c1 = self.c1 + other.c1
+        self.c2 = self.c2 + other.c2
+
+    def neg(self):
+        self.c1 = -self.c1
+        self.c1 = -self.c2
+
+    def neg_c1(self):
+        self.c1 = -self.c1
+
+    def neg_c2(self):
+        self.c2 = -self.c2
+
+class Snake:
+    def __init__(self, snake_length):
+        self.part_list = [NumPair(0, i * BLOCK_SIZE) for i in range(snake_length)]
+        self.dir = NumPair(0, BLOCK_SIZE)
+
+
 
 class Game:
     def __init__(self):
         pygame.init()
         self.size = self.width, self.height = 320, 240
-        self.speed = [1, 1]
         self.black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.size)
         self.snake = Snake()
-        self.surface = pygame.Surface([10, 10])
+        self.surface = pygame.Surface([BLOCK_SIZE, BLOCK_SIZE])
         self.surface.fill((20, 20, 20))
         self.surface.fill((244, 244, 244), self.surface.get_rect().inflate(-1, -1))
 
@@ -38,6 +46,11 @@ class Game:
             return True
         else:
             return False
+    def MoveSnake(self):
+        tail = self.part_list.pop(0)
+        if len(self.part_list) == 1:
+            if self.isColliding(tail.add(self.speed))
+        else:
 
     def play(self):
         while True:
@@ -45,11 +58,13 @@ class Game:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.black)
-            self.snake.move()
-            for part in self.snake.part_list:
-                self.screen.blit(self.surface, part)
+            self.MoveSnake()
+            self.blit()
             pygame.display.flip()
             pygame.time.delay(200)
+    def blit(self):
+        for part in self.snake.part_list:
+            self.screen.blit(self.surface, part)
 
 
 game = Game()
